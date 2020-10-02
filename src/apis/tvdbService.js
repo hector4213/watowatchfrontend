e0a65c1f5c5c6fa9b04d0790f7ba8fad72470a2f
Hector <hectorpwns@gmail.com>
2020-10-02 16:19:35 -0400

    api refactor

--- src/apis/tvdbService.js
+++ src/apis/tvdbService.js
@@ -4,58 +4,48 @@ const tvdbConfig = `https://api.themoviedb.org/3/configuration`
 
 const baseURL = `https://api.themoviedb.org/3/movie`
 
-const apiParams = {
+const config = {
   language: 'en-US',
   api_key: process.env.REACT_APP_TVDB_APIKEY,
 }
 
 const getImgConfig = async () => {
   const response = await axios.get(tvdbConfig, {
-    params: apiParams,
+    params: config,
   })
   return response.data.images
 }
 const getTrending = async () => {
   const response = await axios.get(`${baseURL}/popular`, {
-    params: {
-      apiParams,
-    },
+    params: config,
   })
   return response.data.results
 }
 
 const getTopRated = async () => {
   const response = await axios.get(`${baseURL}/top_rated`, {
-    params: {
-      apiParams,
-    },
+    params: config,
   })
   return response.data.results
 }
 
 const getUpAndComing = async () => {
   const response = await axios.get(`${baseURL}/upcoming`, {
-    params: {
-      apiParams,
-    },
+    params: config,
   })
   return response.data.results
 }
 
 const getMovieDetails = async (id) => {
   const response = await axios.get(`${baseURL}/${id}`, {
-    params: {
-      apiParams,
-    },
+    params: config,
   })
   return response.data
 }
 
 const getRecommendations = async (id) => {
   const response = await axios.get(`${baseURL}/${id}/recommendations`, {
-    params: {
-      apiParams,
-    },
+    params: config,
   })
   return response.data
 }
