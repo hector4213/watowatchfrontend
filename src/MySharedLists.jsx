9e0a29d0b13387ff3b0685b056575224fd97e8f3
hector4213 <hectorpwns@gmail.com>
2020-10-21 00:00:31 -0400

    start shared lists

--- src/MySharedLists.jsx
+++ src/MySharedLists.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { Container, Typography } from '@material-ui/core'
 
 const MySharedLists = () => {
-  return <div>My Shared Lists</div>
+  const [buddyLists, setBuddyLists] = useState([])
+
+  useEffect(() => {})
+  return (
+    <Container>
+      <Typography component='h1' variant='h3'>
+        Buddied User Lists
+      </Typography>
+    </Container>
+  )
 }
 
 export default MySharedLists
