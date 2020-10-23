c98f4197f1ddf4d5a9c8914c32c03e1344477096
hector4213 <hectorpwns@gmail.com>
2020-10-23 06:34:23 -0400

    show buddied lists in shared

--- src/MySharedLists.jsx
+++ src/MySharedLists.jsx
@@ -1,16 +1,45 @@
 import React, { useState, useEffect } from 'react'
+import PosterSlides from './components/PosterSlides'
 
-import { Container, Typography } from '@material-ui/core'
+import listService from './apis/listService'
+import tvdbService from './apis/tvdbService'
 
-const MySharedLists = () => {
+import { Container, Typography, Grid } from '@material-ui/core'
+
+const MySharedLists = ({ user, config, getBuddiedLists }) => {
   const [buddyLists, setBuddyLists] = useState([])
 
-  useEffect(() => {})
+  const fetchBuddiedLists = async () => {
+    const response = await getBuddiedLists(user.id)
+    setBuddyLists(response)
+  }
+
+  useEffect(() => {
+    fetchBuddiedLists()
+  }, [])
   return (
     <Container>
       <Typography component='h1' variant='h3'>
         Buddied User Lists
       </Typography>
+      <Grid container spacing={3}>
+        {buddyLists.map((list) => (
+          <Grid item xs={12}>
+            <p>{list.title}</p>
+            <PosterSlides
+              movieData={list.movies}
+              config={config}
+              hasDelete={true}
+            />
+            {list.buddy_ids.map((buddy) => (
+              <div>
+                <p>{buddy.f1}</p>
+                <p>{buddy.f2}</p>
+              </div>
+            ))}
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   )
 }
