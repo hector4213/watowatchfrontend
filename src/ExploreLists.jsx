2ad4bb433158a46d03267b3b96c96b2b85a2e687
hector4213 <hectorpwns@gmail.com>
2020-10-31 15:54:43 -0400

    styling of list data

--- src/ExploreLists.jsx
+++ src/ExploreLists.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-import { Container, Grid, Typography, Chip } from '@material-ui/core'
-import { Theaters } from '@material-ui/icons'
+import { Container, Grid, Typography, Chip, Paper } from '@material-ui/core'
+import { Theaters, SentimentVeryDissatisfied } from '@material-ui/icons'
 
 import userService from './apis/userService'
 
@@ -21,37 +21,47 @@ const ExploreLists = () => {
 
   return (
     <Container component='main'>
-      <Typography component='h1' variant='h4'>
-        Check out other users collections
-      </Typography>
+      <Paper>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Typography component='h1' variant='h4'>
+              Check out other users collections
+            </Typography>
+          </Grid>
 
-      <Grid container spacing={3}>
-        {users.map((user) => (
-          <>
-            <Grid item xs={12} md={6}>
-              <Typography
-                component={Link}
-                to={`/explore/profile/${user.id}`}
-              >{`${user.first_name}'s Lists`}</Typography>
-            </Grid>
-            <Grid container item xs={12} md={6} spacing={2}>
-              {user.movelists.map((list) =>
-                list === null ? (
-                  'User has no lists'
-                ) : (
-                  <Grid item xs={12}>
-                    <Chip
-                      color='secondary'
-                      icon={<Theaters />}
-                      label={list.title}
-                    />
-                  </Grid>
-                )
-              )}
-            </Grid>
-          </>
-        ))}
-      </Grid>
+          {users.map((user) => (
+            <>
+              <Grid item xs={12} md={6}>
+                <Typography
+                  component={Link}
+                  to={`/explore/profile/${user.id}`}
+                >{`${user.first_name}'s Lists`}</Typography>
+              </Grid>
+              <Grid container item xs={12} md={6} spacing={2}>
+                {user.movelists.map((list) =>
+                  list === null ? (
+                    <Grid item xs={12}>
+                      <Chip
+                        color='primary'
+                        icon={<SentimentVeryDissatisfied />}
+                        label={`User has no lists`}
+                      />
+                    </Grid>
+                  ) : (
+                    <Grid item xs={12}>
+                      <Chip
+                        color='secondary'
+                        icon={<Theaters />}
+                        label={list.title}
+                      />
+                    </Grid>
+                  )
+                )}
+              </Grid>
+            </>
+          ))}
+        </Grid>
+      </Paper>
     </Container>
   )
 }
