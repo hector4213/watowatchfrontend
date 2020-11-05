947996165f7dc3f6c5041113e6b12f047761ffde
hector4213 <hectorpwns@gmail.com>
2020-11-04 23:28:09 -0500

    Put components into mui grid

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
-import { Container, Typography } from '@material-ui/core'
+import { Container, Typography, Grid } from '@material-ui/core'
 import Loader from './components/Loader'
 
 import { TrendingUp, Whatshot, Replay } from '@material-ui/icons'
@@ -17,48 +17,63 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const Explore = ({ trending, config, topRated, upComing, isLoading }) => {
-  console.log(trending)
   const classes = useStyles()
-
   if (isLoading) {
     return <Loader />
   }
   return (
     <Container>
-      <div>
-        <Typography
-          align='left'
-          variant='h3'
-          gutterBottom={true}
-          className={classes.header}
-        >
-          Trending
-          <TrendingUp color='secondary' fontSize='inherit' />
-        </Typography>
-      </div>
-      <div className={classes.sliderContainer}>
-        <PosterSlides movieData={trending} config={config} hasDelete={false} />
-        <Typography
-          align='left'
-          variant='h3'
-          gutterBottom={true}
-          className={classes.header}
-        >
-          Top Rated
-          <Whatshot color='secondary' fontSize='inherit' />
-        </Typography>
-        <PosterSlides movieData={topRated} config={config} hasDelete={false} />
-        <Typography
-          align='left'
-          variant='h3'
-          gutterBottom={true}
-          className={classes.header}
-        >
-          Up and Coming
-          <Replay color='secondary' fontSize='inherit' />
-        </Typography>
-        <PosterSlides movieData={upComing} config={config} hasDelete={false} />
-      </div>
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <Typography variant='h6'>
+            Trending
+            <TrendingUp color='secondary' fontSize='inherit' />
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <PosterSlides
+            movieData={trending}
+            config={config}
+            hasDelete={false}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            align='left'
+            variant='h6'
+            gutterBottom={true}
+            className={classes.header}
+          >
+            Top Rated
+            <Whatshot color='secondary' fontSize='inherit' />
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <PosterSlides
+            movieData={topRated}
+            config={config}
+            hasDelete={false}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            align='left'
+            variant='h6'
+            gutterBottom={true}
+            className={classes.header}
+          >
+            Up and Coming
+            <Replay color='secondary' fontSize='inherit' />
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <PosterSlides
+            movieData={upComing}
+            config={config}
+            hasDelete={false}
+          />
+        </Grid>
+      </Grid>
     </Container>
   )
 }
