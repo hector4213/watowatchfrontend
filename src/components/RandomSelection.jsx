f4ecbbb6db7483f1080585a5c17f83d5d80525fc
hector4213 <hectorpwns@gmail.com>
2020-11-06 14:41:59 -0500

    add component to either show posterslides or random selection if clicked

--- src/components/RandomSelection.jsx
+++ src/components/RandomSelection.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const RandomSelection = () => {
-  return <div>im the selection</div>
+import LoadWinner from './LoadWinner'
+
+import PosterSlides from './PosterSlides'
+
+const RandomSelection = ({ basket, config, isBasket, handleBasketDelete }) => {
+  if (basket.length === 1 && basket.some((movie) => movie.winner)) {
+    return <LoadWinner winner={[...basket]} config={config} />
+  }
+  return (
+    <PosterSlides
+      movieData={basket}
+      config={config}
+      isBasket={isBasket}
+      handleBasketDelete={handleBasketDelete}
+    />
+  )
 }
 
 export default RandomSelection
