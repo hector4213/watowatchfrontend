8c84a441a563d1a937613f1ebfbf24591caab8e1
hector4213 <hectorpwns@gmail.com>
2020-11-13 13:36:09 -0500

    remove unused

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
 
-import { makeStyles } from '@material-ui/core/styles'
-import { Container, Typography, Grid } from '@material-ui/core'
+import { Container } from '@material-ui/core/'
+
 import Loader from './components/Loader'
 
-import { TrendingUp, Whatshot, Replay } from '@material-ui/icons'
-
-import PosterSlides from './components/PosterSlides'
 import ExploreGrid from './components/ExploreGrid'
 
-const useStyles = makeStyles((theme) => ({
-  root: {},
-  header: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-}))
-
 const Explore = ({ trending, config, topRated, upComing, isLoading }) => {
-  const classes = useStyles()
   if (isLoading) {
     return <Loader />
   }
