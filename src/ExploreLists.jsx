8ae63a8922cdbb708b4630c843b2aa698f0f8d3e
hector4213 <hectorpwns@gmail.com>
2020-11-17 14:02:19 -0500

    refactor out to component

--- src/ExploreLists.jsx
+++ src/ExploreLists.jsx
@@ -1,17 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-import {
-  Container,
-  Grid,
-  Typography,
-  Paper,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-} from '@material-ui/core'
-import { ListAltRounded } from '@material-ui/icons'
+import { Container, Grid, Typography, Paper } from '@material-ui/core'
+
+import UserListInfo from './components/UserListInfo'
 
 import userService from './apis/userService'
 
@@ -32,33 +24,14 @@ const ExploreLists = () => {
       <Paper>
         <Grid container spacing={3} justify='center'>
           <Grid item xs={12}>
-            <Typography component='h1' variant='h4'>
+            <Typography component='h1' variant='h6'>
               Check out other users collections
             </Typography>
           </Grid>
-
           {users.map((user) => (
-            <>
-              <Grid item xs={12} md={6}>
-                <Typography
-                  variant='overline'
-                  component={Link}
-                  to={`/explore/profile/${user.id}`}
-                >{`${user.first_name}'s Lists`}</Typography>
-              </Grid>
-              <Grid container item xs={12} md={6} spacing={2}>
-                <List>
-                  {user.movelists.map((list) => (
-                    <ListItem>
-                      <ListItemIcon>
-                        <ListAltRounded />
-                      </ListItemIcon>
-                      <ListItemText primary={list.title} />
-                    </ListItem>
-                  ))}
-                </List>
-              </Grid>
-            </>
+            <Grid item xs={12} md={6}>
+              <UserListInfo user={user} />
+            </Grid>
           ))}
         </Grid>
       </Paper>
